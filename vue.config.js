a2bf2941fbf43fa25a0cb3fea5a185f5f9e15ed4
jack <690137092@qq.com>
2019-12-18 21:01:42 +0800

    add

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2019-12-05 14:07:48
+ * @LastEditTime: 2019-12-18 20:15:46
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: \studyd:\新建文件夹\vue-sweet-calendar\vue.config.js
+ */
 module.exports = {
   publicPath: './',
   lintOnSave: false
