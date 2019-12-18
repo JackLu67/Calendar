198928ed58594993f22b9e150c3adca2c66fe57f
jack <690137092@qq.com>
2019-12-18 20:59:43 +0800

    增加字体

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
+/*
+ * @Author: your name
+ * @Date: 2019-12-05 14:07:48
+ * @LastEditTime: 2019-12-18 20:16:59
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: \studyd:\新建文件夹\vue-sweet-calendar\src\main.js
+ */
 import Vue from 'vue'
 import App from './App.vue'
-
+import './assets/font/font.css'
 Vue.config.productionTip = false
 
 new Vue({
